cccc60a17dddc80284f93295423a85b989451504
Paras Nagda <pnagda@codeaurora.org>
2015-09-16 15:34:22 +0530

    msm8974: Add support for 'frame-rate' key
    Create new media_codecs_performace.xml to add
    frame-rate key support required for video perf
    cts tests.
    
    Change-Id: Ia42283596c986539f8ce7b7744932aeacad7aadd

--- nx503a.mk
+++ nx503a.mk
@@ -173,6 +173,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/etc/media_codecs_8974.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/etc/media_codecs_performance_8974.xml:system/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/etc/media_profiles_8974.xml:system/etc/media_profiles.xml
     
 # Media
