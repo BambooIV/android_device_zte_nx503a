982f6e52ef3746d211d2d74b6e26d04d37df1d7c
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-10-09 17:59:43 +0200

    add libboringssl-compat
    enable libboringssl-compat needed by old adreno/camera blobs
    
    Change-Id: If8d77c4e76ea71fc5dd2ea4692d9cd807f54f021
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- nx503a.mk
+++ nx503a.mk
@@ -114,6 +114,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/mixer_paths_auxpcm.xml:system/etc/mixer_paths_auxpcm.xml
 
+# BoringSSL hacks
+PRODUCT_PACKAGES += \
+    libboringssl-compat
+
 # Camera
 PRODUCT_PACKAGES += \
     libxml2 \
