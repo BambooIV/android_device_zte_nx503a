3787b76dad35b751cf23c3fbfd31ef88254b8149
BambooIV <ningdisheng@gmail.com>
2016-04-05 21:48:23 +0800

    gps: Disable qcom network location for now
    * Dirty fix GPS

--- nx503a.mk
+++ nx503a.mk
@@ -155,10 +155,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.msm8974
 
-# GPS
-PRODUCT_PACKAGES += \
-    gps.msm8974
-
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
