device-tree/android_device_nubia_nx503a

nx503a: Add CameraWrapper
* Import from g2-common

BoardConfig.mk
@@ -150,7 +150,7 @@ BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
-TARGET_PROVIDES_CAMERA_HAL := true
+COMMON_GLOBAL_CFLAGS += -DLG_CAMERA_HARDWARE
 
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)

BambooIV/android_device_zte_nx503a

BoardConfig.mk
@@ -81,7 +81,7 @@ BOARD_BLUEDROID_VENDOR_CONF := device/zte/nx503a/bluetooth/libbt_vndcfg.txt
 
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
-TARGET_PROVIDES_CAMERA_HAL := true
+COMMON_GLOBAL_CFLAGS += -DOPPO_CAMERA_HARDWARE
 
 # Charger
 BOARD_CHARGER_SHOW_PERCENTAGE := true