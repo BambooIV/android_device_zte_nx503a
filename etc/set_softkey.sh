8c2238453d2c2e6e289711af0847048000546ec8
BambooIV <ningdisheng@gmail.com>
2016-04-11 22:27:09 +0800

    nx503a: Fix KeyDisabler
    - Fix KeyDisabler
    
    - Hack settings strings to inform that
      need to reboot

--- etc/set_softkey.sh
+++ etc/set_softkey.sh
@@ -4,7 +4,7 @@
 sleep 2
 softkey=`settings get secure dev_force_show_navbar`
 file="/system/usr/keylayout/Generic.kl"
-to_do="$(cat $file | grep "key 102   HOME" | cut -b1)"
+to_do="$(cat $file | grep "key 102   HOME              VIRTUAL" | cut -b1)"
 
 mount -o rw,remount /system
 
@@ -12,17 +12,17 @@ case "$softkey" in
 	"0")
 		if [ "$to_do" == "#" ]
 		then
-			sed -i "s/#key 102   HOME/key 102   HOME/" $file
-			sed -i "s/#key 139   MENU/key 139   MENU/" $file
+			sed -i "s/#key 102   HOME              VIRTUAL/key 102   HOME              VIRTUAL/" $file
+			sed -i "s/#key 139   MENU              VIRTUAL/key 139   MENU              VIRTUAL/" $file
 			sed -i "s/#key 158   BACK/key 158   BACK/" $file
 		fi
 		;;
 	"1")
 		if [ "$to_do" != "#" ]
 		then
-			sed -i "s/key 102   HOME/#key 102   HOME/" $file
-			sed -i "s/key 139   MENU/#key 139   MENU/" $file
-			sed -i "s/key 158   BACK/#key 158   BACK/" $file
+			sed -i "s/key 102   HOME              VIRTUAL/#key 102   HOME              VIRTUAL/" $file
+			sed -i "s/key 139   MENU              VIRTUAL/#key 139   MENU              VIRTUAL/" $file
+			sed -i "s/key 158   BACK              VIRTUAL/#key 158   BACK              VIRTUAL/" $file
 		fi
 		;;
 esac
