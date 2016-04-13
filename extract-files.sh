727f971559e69943ea264b28edfc9ce5de5cd256
BambooIV <ningdisheng@gmail.com>
2016-04-13 15:04:09 +0800

    nx503a: Bring back proprietary-files.txt
    * Do not run extract-files.sh,
      that's just a memo

---
+++ extract-files.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export VENDOR=zte
+export DEVICE=nx503a
+
+../../../vendor/extra/tools/extract-files.sh $@
---
+++ setup-makefiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export VENDOR=zte
+export DEVICE=nx503a
+
+../../../vendor/extra/tools/setup-makefiles.sh
