b3bf0f08687c0dc670b4e9ef32492459a20efb1f
BambooIV <ningdisheng@gmail.com>
2016-04-13 14:43:21 +0800

    nx503a: Build init vendor lib

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,6 +137,9 @@ MAX_EGL_CACHE_KEY_SIZE := 12*1024
 # of the device.
 MAX_EGL_CACHE_SIZE := 2048*1024
 
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_msm
+
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
