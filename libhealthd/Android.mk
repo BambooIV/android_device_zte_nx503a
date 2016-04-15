82783c78ac5ddca99a5271d97db3679f4dd9413e
Arne Coucheron <arco68@gmail.com>
2016-04-15 07:00:16 +0200

    healthd: Rename libhealthd library
    * Avoid possible module name conflicts
    
    * CyanogenMod/android_system_core@4ca66c4
    * CyanogenMod/android_system_core@05a3648
    
    Change-Id: I293d5f7129e869b083297670b5b519a33aca4d8e

--- libhealthd/Android.mk
+++ libhealthd/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := healthd_board_default.cpp
-LOCAL_MODULE := libhealthd.qcom
+LOCAL_MODULE := libhealthd.msm
 LOCAL_C_INCLUDES := \
     system/core/healthd \
     bootable/recovery
