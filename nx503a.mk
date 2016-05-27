911ae76936cb743bb51d37239b01b524abfd23f3
BambooIV <ningdisheng@gmail.com>
2016-05-27 21:25:18 +0800

    nx503a: perf: Change ro property value
    libqc-opt is changed to libqti-perfd-client
    for unification.

--- nx503a.mk
+++ nx503a.mk
@@ -326,7 +326,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=/vendor/lib/libqc-opt.so \
+    ro.vendor.extension_library=libqti-perfd-client.so \
     persist.radio.apm_sim_not_pwdn=1 \
     ro.com.google.clientidbase=android-zte \
     ro.com.google.clientidbase.ms=android-zte \
