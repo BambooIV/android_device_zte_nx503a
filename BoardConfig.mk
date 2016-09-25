93be5b8f3d58f475f2e4e69f21a1e3f45ff359ab
BambooIV <ningdisheng@hotmail.com>
2016-09-25 09:47:21 +0800

    bionic: libc: Add flag to disable clang
    Adds the flag TARGET_BIONIC_DISABLE_CLANG for enabling/disabling
    clang when needed.
    
    https://github.com/AOSPA/android_bionic/commit/a6d1d727c0bb448ab25ee97c84592bd0c91d0441

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,6 +89,9 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 # CM Hardware
 BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw/
 
+# Disable clang
+TARGET_BIONIC_DISABLE_CLANG := true
+
 # Disable memcpy_base.S optimization
 TARGET_CPU_MEMCPY_BASE_OPT_DISABLE := true
 
